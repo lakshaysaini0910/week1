706ddd01481f629a95512cd42ee52f44fef0f12e
lakshaysaini0910 <lakshaysaini0910@gmail.com>
2025-12-12 01:04:07 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,10 @@
+print("Welcome! Let's get to know you \n")
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+city = input("Which city do you live in? ")
+
+print("\n Welcome", name + "! ")
+print(f"You are {age} years old \nYou live in {city} \nYou love {hobby}.")
+print("Glad to have you here!")
